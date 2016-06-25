a274286506997b23b48dadf593f4fc2dc38a25f7
Jeevan James <jeevanjj@yahoo.com>
2016-06-26 05:04:10 +0530

    Initial commit.

---
+++ SolutionInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jeevan James")]
+[assembly: AssemblyProduct("FullStackTraining")]
+[assembly: AssemblyCopyright("Copyright © Jeevan James 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0-build0003")]
