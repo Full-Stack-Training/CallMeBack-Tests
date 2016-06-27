73db7ae9fc4d20ea2dac66f08d4ec0eb3be68b2b
Jeevan James <jeevanjj@yahoo.com>
2016-06-27 10:12:20 +0530

    Added fixtures for container and user to the shared project.

---
+++ Shared/ContainerFixture.cs
@@ -0,0 +1,19 @@
+﻿using Basics.Containers;
+using Basics.Testing.Xunit;
+
+using FullStackTraining.CallMeBack.Domain;
+
+namespace FullStackTraining.CallMeBack.Tests.Shared
+{
+    public sealed class ContainerFixture : ContainerFixtureBase
+    {
+        public ContainerFixture() : base(Registration)
+        {
+        }
+
+        private static void Registration(IContainerBuilder builder)
+        {
+            ContainerSetup.Setup(builder);
+        }
+    }
+}
---
+++ Shared/UserFixture.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+using Basics.Testing.Xunit;
+
+using FullStackTraining.CallMeBack.Domain.Contracts;
+
+namespace FullStackTraining.CallMeBack.Tests.Shared
+{
+    public sealed class UserFixture : UserContextFixtureBase
+    {
+        protected override IEnumerable<string> GetAdminPermissions()
+        {
+            yield return Permissions.RegisterCallbackNumbers;
+            yield return Permissions.SearchCallbackNumbers;
+
+            yield return Permissions.RegisterFavorites;
+            yield return Permissions.SearchFavorites;
+        }
+
+        protected override IEnumerable<string> GetUserPermissions()
+        {
+            yield return Permissions.SearchCallbackNumbers;
+            yield return Permissions.SearchFavorites;
+        }
+    }
+}
